9ad7dff49214adf3a16f17aa570f42d357afe3b1
Cristhian Andres Cuenca Trujillo <ccuenca@imbanaco.com.co>
2022-01-22 11:51:59 -0500

    Catalog API - Data Acces Layer Implemented

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+  public interface ICatalogContext
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    IMongoCollection<Product> Products { get; }
+  }
+}
