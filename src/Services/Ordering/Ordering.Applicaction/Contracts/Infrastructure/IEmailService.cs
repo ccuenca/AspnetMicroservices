fbdd8f6afb977b4d2c90f8e6f13afaa48e88448e
Cristhian Andres Cuenca Trujillo <ccuenca@imbanaco.com.co>
2022-02-08 08:23:03 -0500

    Appication Behaviors on Meditr ompleted

--- src/Services/Ordering/Ordering.Applicaction/Contracts/Infrastructure/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ordering.Applicaction.Model;
-
-namespace Ordering.Applicaction.Contracts.Infrastructure
-{
-  public interface IEmailService
-  {
-    Task<bool> SendEmail(Email email);
-  }
-}
---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Ordering.Application.Model;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+  public interface IEmailService
+  {
+    Task<bool> SendEmail(Email email);
+  }
+}
