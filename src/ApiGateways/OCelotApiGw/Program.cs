c328ef23ba33611a4604d00ce4bf3e31b8c56bd8
ccuenca <ccuenca24@gmail.com>
2022-02-26 13:43:34 -0500

    ApiGw on Docker Environment Completed

--- src/ApiGateways/OCelotApiGw/Program.cs
+++ src/ApiGateways/OCelotApiGw/Program.cs
@@ -1,5 +1,6 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,11 +16,15 @@ var host = builder.Host
         logginbuilder.AddDebug();
     });
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+    .AddCacheManager(settings =>
+    {
+        settings.WithDictionaryHandle();
+    });
 
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!");
 
 await app.UseOcelot();
 
